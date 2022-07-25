a9a2fa64229cbb62991fe8bc733ba276920b655d
EmLanc2715 <101427781+EmLanc2715@users.noreply.github.com>
2022-07-25 15:10:33 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -18,6 +18,7 @@ ORDER BY e.emp_no
 
 --Confirm that the new table looks like the one in challenge documentation
 SELECT * FROM retirement_titles
+SELECT COUNT (*) FROM retirement_titles
 
 -- Use Dictinct with Orderby to remove duplicate rows
 --Create a unique_titles table
@@ -65,4 +66,45 @@ AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY t.emp_no;
 
 SELECT * FROM mentorship_eligibilty
+
+--Analysis
+
+select count(distinct e.emp_no)
+from employees e 
+inner join dept_emp t on (e.emp_no=t.emp_no)
+--300024 employees
+
+select count(distinct e.emp_no)
+from employees e 
+inner join dept_emp t on (e.emp_no=t.emp_no)
+and t.to_date='9999-01-01'
+--240124 current employees
+
+select * 
+into all_year_titles 
+from 
+(select distinct extract(YEAR from birth_date) birth_year 
+from employees) e
+cross join
+(select distinct title 
+from titles) t;
+
+select t.title,extract(YEAR from e.birth_date) birth_year,count(*) tot_cnt
+into emp_year_titles 
+from 
+(SELECT DISTINCT ON (emp_no) emp_no, title
+FROM titles
+WHERE to_date='9999-01-01'
+ORDER BY emp_no, to_date DESC) t
+inner join employees e on (e.emp_no=t.emp_no)
+group by t.title,extract(YEAR from e.birth_date)
+order by t.title,extract(YEAR from e.birth_date)
+;
+
+SELECT COUNT (*) From mentorship_eligibilty
+--1549 employees eligible for mentorship
+
+SELECT * FROM retirement_titles
+
+--Additional Queries
 
